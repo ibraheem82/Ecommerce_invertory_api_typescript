7fc867ff8e0adf1d8e88ffebb2c59a191823691e
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-20 04:19:22 +0100

    completed the inventory product validations.....

--- src/modules/products/product.validation.ts
+++ src/modules/products/product.validation.ts
@@ -10,19 +10,22 @@ export const inventoryValidationSchema = z.object({
     inStock: z.boolean(),
   });
 
+
 const productValidationSchema = z.object({
-    name: z.string({
-        required_error: "Name of Product is required",
-        invalid_type_error: "Name must be a string",
-      }),
-    description: z.string(),
-    // price	Positive number	> 0
-    price: z.number().positive(),
-    category: z.string(),
-    // Array of strings	z.array() validates arrays
-    tags: z.array(z.string()),
-    variants: z.array(variantValidationSchema),
-    inventory: inventoryValidationSchema
-  });
- 
-  export default productValidationSchema;
+  name: z.string({
+    required_error: "Name of Product is required",
+    invalid_type_error: "Name must be a string",
+  }),
+
+  description: z.string(),
+
+  // price -> Positive number > 0
+  price: z.number().positive(),
+  category: z.string(),
+// Array of string z.array() validates arrays 
+  tags: z.array(z.string()),
+  variants: z.array(variantValidationSchema),
+  inventory: inventoryValidationSchema
+});
+
+export default productValidationSchema;
