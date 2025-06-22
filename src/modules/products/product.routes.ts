d9468027b23b615f92154e23bb17ac25c53cba2b
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-22 11:04:15 +0100

    ``` feat: Configure product routes
    This commit sets up the API routes for product-related operations.
    
    Key changes include:
    - Initializes an Express router.
    - Defines a **POST** route at `/` to `ProductControllers.createProduct` for creating new products.
    - Defines a **GET** route at `/` to `ProductControllers.getAllProducts` for retrieving all products.
    ```

---
+++ src/modules/products/product.routes.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { ProductControllers } from "./product.controller";
+// import { verfiyToken } from "../../middleware/authMiddleware";
+// import { isAdmin } from "../../middleware/adminMiddleware";
+const router = express.Router();
+
+router.post('/', ProductControllers.createProduct)
+router.get('/', ProductControllers.getAllProducts)
+// router.post('/',verfiyToken, isAdmin,  ProductControllers.createProduct)
+// router.get("/:productId",ProductControllers.getSingleProduct)
+// router.put("/:productId", verfiyToken, isAdmin, ProductControllers.updateProduct)
+// router.delete("/:productId", verfiyToken, isAdmin, ProductControllers.deleteProduct)
+
+
+export const ProductRoutes = router;
