ccb123ece714ba504386d0d35ed7ffbcdd742b7b
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-22 10:48:16 +0100

    ``` feat: Add create product controller
    This commit introduces the `createProduct` controller, responsible for handling incoming requests to create new products.
    
    Key changes include:
    - Imports `Request` and `Response` from Express, `productValidationSchema` for input validation, and `ProductServices` for database interaction.
    - Implements `createProduct` to:
        - Validate request body against `productValidationSchema` using Zod.
        - Call `ProductServices.createAProductIntoDB` with the validated data.
        - Send a success response with the created product data on successful creation.
        - Catch and handle errors, sending appropriate error responses.
    - Exports `ProductControllers` containing the new `createProduct` function.
    ```

---
+++ src/modules/products/product.controller.ts
@@ -0,0 +1,34 @@
+import { Request, Response } from "express"
+import productValidationSchema from "./product.validation";
+import { ProductServices } from "./product.services";
+
+
+
+const createProduct = async (req: Request, res: Response) => {
+   try {
+
+    const zodParser = productValidationSchema.parse(req.body);
+    const result = await ProductServices.createAProductIntoDB(zodParser);
+     res.status(200).json({
+        success: true,
+        message: "Product created successfully",
+        data: result
+    })
+    } catch (err: any) {
+    res.status(500).json({
+        success: false,
+        message: err.message ||  "Something went wrong",
+        error: err
+    })
+    }
+
+}
+
+
+export const ProductControllers = {
+    createProduct
+    // getAllProducts,
+    // getSingleProduct,
+    // updateProduct,
+    // deleteProduct
+}
