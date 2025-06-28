447934e56e1c3540f1310a9cadd4ea20880cd38c
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-28 05:53:55 +0100

    rebuilt it...

--- dist/app.js
+++ dist/app.js
@@ -11,4 +11,7 @@ app.use(cors());
 app.use('/api/products', ProductRoutes);
 app.use('/api/orders', OrderRoutes);
 app.use('/api/users', UserRoutes);
+app.get('/', (req, res) => {
+    res.send('Ecommerce Inventory Server is running..!');
+});
 export default app;
