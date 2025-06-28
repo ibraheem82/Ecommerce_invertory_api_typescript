ab49208bcee53484f24cd47beb8e692ae872054e
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-28 03:56:18 +0100

    folder structure changed....

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import express, {json} from 'express';
 import cors from "cors";
-import { ProductRoutes } from './modules/products/product.routes';
-import { OrderRoutes } from './modules/orders/order.routes';
-import { UserRoutes } from './modules/users/user.routes';
+import { ProductRoutes } from './app/modules/products/product.routes';
+import { OrderRoutes } from './app/modules/orders/order.routes';
+import { UserRoutes } from './app/modules/users/user.routes';
 const app = express();
 
 
