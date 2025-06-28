63a99c41e6d1b0e1496d1c321fe434f1f35e93e7
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-28 05:08:28 +0100

    mounted the verifyToken and admin middleware here ...

--- src/app/modules/products/product.routes.ts
+++ src/app/modules/products/product.routes.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import { ProductControllers } from "./product.controller";
-// import { verfiyToken } from "../../middleware/authMiddleware";
-// import { isAdmin } from "../../middleware/adminMiddleware";
+import { verfiyToken } from "../../middleware/authMiddleware";
+import { isAdmin } from "../../middleware/adminMiddleware";
 const router = express.Router();
 
-router.post('/', ProductControllers.createProduct)
+router.post('/', verfiyToken, isAdmin, ProductControllers.createProduct)
 router.get('/', ProductControllers.getAllProducts)
 router.get("/:productId",ProductControllers.getSingleProduct)
-router.put("/:productId", ProductControllers.updateProduct)
-// router.post('/',verfiyToken, isAdmin,  ProductControllers.createProduct)
-router.delete("/:productId", ProductControllers.deleteProduct)
+router.put("/:productId", verfiyToken, ProductControllers.updateProduct)
+router.delete("/:productId", verfiyToken, isAdmin, ProductControllers.deleteProduct)
 
 
 export const ProductRoutes = router;
