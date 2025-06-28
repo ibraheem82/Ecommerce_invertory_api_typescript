846f971a53703091f6836c6074f604a1a65fd9eb
ibraheem82 <ibraheemomikunle82@gmail.com>
2025-06-28 03:53:03 +0100

    erverthing was deleted...

--- src/modules/orders/order.interface.ts
+++
@@ -1,6 +0,0 @@
-export type TOrder = {
-    email: string;
-    productId: string;
-    quantity: number;
-    price: number;
-}
--- src/modules/users/user.routes.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { UserController } from "./user.controller";
-const router = express.Router();
-
-router.post("/create-account", UserController.registerUser);
-
-router.post("/login", UserController.loginUser);
-
-export const UserRoutes = router;
